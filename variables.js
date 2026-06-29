f4457d5312ffcfa6dbc031d656313f78dc2f4bb2
4nik2r24-sketch <4nik2r24@gmail.com>
2026-06-29 19:26:37 -0400

    first commit

---
+++ variables.js
@@ -0,0 +1,7 @@
+const nombre = "Mauricio";
+const edad = 43;
+const enServicio = true;
+
+console.log(`Valor: ${nombre} | Tipo: ${typeof nombre}`);
+console.log(`Valor: ${edad} | Tipo: ${typeof edad}`);
+console.log(`Valor: ${enServicio} | Tipo: ${typeof enServicio}`);
